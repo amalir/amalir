fe61c9ef46903a2591d155e6b2dd9cc7deb93fae
Adam Malíř <astromalir@gmail.com>
2020-12-13 19:23:21 +0100

    Create quicksort.cs

---
+++ justforfun/quicksort.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MainClass {
+
+  static List < int > arr;
+
+  public static void Main(string[] args) {
+
+    arr = Sort(new List < int > (new int[] {
+      12,
+      13,
+      14,
+      25,
+      14,
+      14,
+      78,
+      12,
+      34,
+      124,
+      16
+    }));
+    Vypis();
+
+  }
+
+  static List < int > Sort(List < int > arr) {
+
+    List < int > left = new List < int > ();
+    List < int > right = new List < int > ();
+
+    int mx = arr.Count - 1;
+    int pivot = arr[mx];
+
+    for (int i = 0; i < mx; i++) {
+      if (arr[i] > pivot) right.Add(arr[i]);
+      else left.Add(arr[i]);
+    }
+
+    left = (left.Count > 0) ? Sort(left) : left;
+    right = (right.Count > 0) ? Sort(right) : right;
+
+    left.Add(pivot);
+    left.AddRange(right);
+
+    return left;
+
+  }
+
+  static void Vypis() {
+
+    foreach(int i in arr) {
+      Console.Write(i + " ");
+    }
+    Console.WriteLine("");
+
+  }
+
+}
