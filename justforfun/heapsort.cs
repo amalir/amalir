1af5c5a55715335c94012d149aaa72094a3706be
Adam Malíř <astromalir@gmail.com>
2020-12-13 19:23:57 +0100

    Update heapsort.cs

--- justforfun/heapsort.cs
+++ justforfun/heapsort.cs
@@ -3,49 +3,59 @@ using System.Linq;
 
 class MainClass {
 
-static int[] arr = {12,13,14,25,14,14,78,12,34,124,16};
+  static int[] arr = {
+    12,
+    13,
+    14,
+    25,
+    14,
+    14,
+    78,
+    12,
+    34,
+    124,
+    16
+  };
 
-public static void Main (string[] args) {
+  public static void Main(string[] args) {
 
-Vypis();
-Porovnej(arr.Length);
-Vypis();
+    Vypis();
+    Porovnej(arr.Length);
+    Vypis();
 
-}
+  }
 
-public static void Porovnej(int pocet) {
+  static void Porovnej(int pocet) {
 
-int i = 0;
-int mx = 2*i;
+    int i = 0;
+    int mx = 2 * i;
 
-while (i<pocet/2) {
-mx = (arr[2*i+1]<arr[mx]) ? 2*i+1 : mx;
- if(2*i+2<pocet)
-  mx = (arr[2*i+2]<arr[mx]) ? 2*i+2 : mx;
-i++;
-}
+    while (i < pocet / 2) {
+      mx = (arr[2 * i + 1] < arr[mx]) ? 2 * i + 1 : mx;
+      if (2 * i + 2 < pocet) mx = (arr[2 * i + 2] < arr[mx]) ? 2 * i + 2 : mx;
+      i++;
+    }
 
-Prohod(mx,pocet-1);
-pocet--;
-if(pocet!=1)
- Porovnej(pocet);
+    Prohod(mx, pocet - 1);
+    pocet--;
+    if (pocet != 1) Porovnej(pocet);
 
-}
+  }
 
-public static void Prohod(int x,int y) {
+  static void Prohod(int x, int y) {
 
-  int c = arr[x];
-  arr[x] = arr[y];
-  arr[y] = c;
+    int c = arr[x];
+    arr[x] = arr[y];
+    arr[y] = c;
 
-}
+  }
 
-public static void Vypis() {
+  static void Vypis() {
 
-   foreach (int i in arr) {
-    Console.Write(i+" ");
- }
- Console.WriteLine(""); 
+    foreach(int i in arr) {
+      Console.Write(i + " ");
+    }
+    Console.WriteLine("");
 
- }
+  }
 }
